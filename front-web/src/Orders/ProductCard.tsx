628f7e3ea7e6657cfbf78b316a85d16bef16ea34
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2021-01-10 14:05:22 -0300

    Finish front web

--- front-web/src/Orders/ProductCard.tsx
+++ front-web/src/Orders/ProductCard.tsx
@@ -1,37 +1,31 @@
+import { formatPrice } from './helpers';
 import { Product } from './types';
 
 type Props = {
-  product: Product
-}
+  product: Product;
+  onSelectProduct: (product: Product) => void;
+  isSelected: boolean;
+};
 
-function formatPrice(price: number) {
-  const formater = new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-    minimumFractionDigits: 2
-  });
-
-  return formater.format(price)
-}
-
-function ProductCard({ product }: Props) {
+function ProductCard({ product, onSelectProduct, isSelected }: Props) {
   return (
-    <div className="order-card-container">
-      <h3 className="order-card-title">
-        { product.name }
-      </h3>
+    <div
+      className={`order-card-container ${isSelected ? 'selected' : ''}`}
+      onClick={() => onSelectProduct(product)}
+    >
+      <h3 className="order-card-title">{product.name}</h3>
 
-      <img src={ product.imageUri } alt={ product.name } className="order-card-image"/>
+      <img
+        src={product.imageUri}
+        alt={product.name}
+        className="order-card-image"
+      />
 
-      <h3 className="order-card-price">
-        { formatPrice(product.price) }
-      </h3>
+      <h3 className="order-card-price">{formatPrice(product.price)}</h3>
 
       <div className="order-card-description">
         <h3>Descrição</h3>
-        <p>
-          { product.description }
-        </p>
+        <p>{product.description}</p>
       </div>
     </div>
   );
--- front-web/src/Orders/ProductsList.tsx
+++ front-web/src/Orders/ProductsList.tsx
@@ -1,17 +1,25 @@
 import ProductCard from './ProductCard';
 import { Product } from './types';
+import { checkIsSelected } from './helpers';
 
 type Props = {
   products: Product[];
-}
+  onSelectProduct: (product: Product) => void;
+  selectedProducts: Product[];
+};
 
-function ProductsList({ products }: Props) {
+function ProductsList({ products, selectedProducts, onSelectProduct }: Props) {
   return (
     <div className="orders-list-container">
       <div className="orders-list-items">
-        { products.map(product => (
-          <ProductCard key={ product.id } product={ product }/>
-        )) }
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            onSelectProduct={onSelectProduct}
+            isSelected={checkIsSelected(selectedProducts, product)}
+          />
+        ))}
       </div>
     </div>
   );
